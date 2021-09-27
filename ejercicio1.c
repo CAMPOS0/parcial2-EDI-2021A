d0a4738dd3bbdef1447a12c2834b3ab2556f7848
CAMPOS0 <89058881+CAMPOS0@users.noreply.github.com>
2021-09-26 20:39:06 -0500

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <math.h>
+//Ejercicio 2. Escribir un programa en C que genere una matriz de N x N y la llene con 0 excepto la diagonal principal que deberá tener 1.
+#define n 10
 int main()
 {
+    int i,j,r;
+    int M[n][n];
 
-    return 0;
+    printf("Dame el numero\n");
+    scanf("%d", &r);
+    
+    for (i = 0; i < r ; i++)
+    {
+        for (j = 0; j < r; j++)
+        {
+           M[i][j]= 0;
+        
+         if (i==j)
+         {
+            M[i][j] = 1;
+         }
+         printf(" %d ", M[i][j]);
+        }
+        printf("\n");
+    }
+ return 0;
 }
